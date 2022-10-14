d91d846b7aa705b468c2d27eb74b2d7502a05d89
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-10-15 02:29:13 +0300

    native-setup

--- src/main/java/com/example/livemobileapp/model/User.java
+++ src/main/java/com/example/livemobileapp/model/User.java
@@ -2,19 +2,23 @@ package com.example.livemobileapp.model;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.ZonedDateTime;
+import java.time.LocalDateTime;
 @Document
 @Data
 public class User {
     @Id
     private String id;
     private String password;
+    @Indexed(unique = true)
     private String username;
+    @Indexed(unique = true)
+    private String email;
     private String firstname;
     private String lastname;
     private String gender;
-    private String email;
-    private ZonedDateTime createdAt;
+
+    private LocalDateTime createdAt;
 }
