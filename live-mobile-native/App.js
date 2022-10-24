24bb323b796b3af0a59888b80c5615ddafa9745e
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-10-25 02:23:50 +0300

    camera

--- live-mobile-native/App.js
+++ live-mobile-native/App.js
@@ -4,18 +4,18 @@ import { SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import AuthNavigator from "./components/Navigators/AuthNavigator";
 export default function App() {
-  return (
-    <NavigationContainer>
-      <AuthNavigator></AuthNavigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <AuthNavigator></AuthNavigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
