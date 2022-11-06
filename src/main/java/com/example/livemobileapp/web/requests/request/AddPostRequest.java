c44cd7c0bb64bbc50951ddda6d50abb787af75ea
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-11-06 17:59:31 +0300

    AddPostRequest init

---
+++ src/main/java/com/example/livemobileapp/web/requests/request/AddPostRequest.java
@@ -0,0 +1,10 @@
+package com.example.livemobileapp.web.requests.request;
+
+import lombok.Data;
+
+@Data
+public class AddPostRequest {
+    private String userId;
+    private String username;
+    private String content;
+}
