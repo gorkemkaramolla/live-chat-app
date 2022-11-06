76dec2ca08c34f01b5ee4b4f0d0ad94188ab3474
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-11-06 17:57:38 +0300

    Responses Handled

--- src/main/java/com/example/livemobileapp/controller/UserController.java
+++ src/main/java/com/example/livemobileapp/controller/UserController.java
@@ -5,20 +5,16 @@ import com.example.livemobileapp.web.requests.request.UserCreateRequest;
 import com.example.livemobileapp.web.requests.request.UserInformationsRequest;
 import com.example.livemobileapp.web.requests.response.UserInfoResponse;
 import lombok.AllArgsConstructor;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.util.StreamUtils;
+
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -26,27 +22,25 @@ import java.io.InputStream;
 public class UserController {
     private final UserService userService;
     @GetMapping("/{page}")
-    public ResponseEntity getUsers(@PathVariable Integer page)
+    public ResponseEntity<List<UserInfoResponse>> getUsers(@PathVariable Integer page)
     {
         return new ResponseEntity<>(userService.getUsers(page),HttpStatus.OK);
     }
     @PostMapping("/signup")
-    public ResponseEntity save(@RequestBody UserCreateRequest userCreateRequest)
+    public ResponseEntity<UserInfoResponse> save(@RequestBody UserCreateRequest userCreateRequest)
     {
         return new ResponseEntity<>(userService.saveSampleUser(userCreateRequest),HttpStatus.CREATED);
     }
     @PutMapping("/info")
-    public ResponseEntity updateInfo(@RequestBody UserInformationsRequest userInformationsRequest)
+    public ResponseEntity<String> updateInfo(@RequestBody UserInformationsRequest userInformationsRequest)
     {
         UserInfoResponse userInfoResponse = userService.updateInfo(userInformationsRequest);
-        if(userInfoResponse == null)
-        {
-            return new ResponseEntity<>( new UsernameNotFoundException("Error, you can't update user informations"),HttpStatus.BAD_REQUEST);
-        }
-        else {
-            return new ResponseEntity<>("User Updated",HttpStatus.OK);
 
-        }
+        return (userInfoResponse==null?
+                new ResponseEntity<>( "Error, you can't update user information",HttpStatus.BAD_REQUEST):
+                new ResponseEntity<>("User Updated",HttpStatus.OK));
+
+
     }
     @PutMapping("/upload")
     public void singleFileUpload(@RequestParam() MultipartFile file, @RequestParam() String username) throws IOException {
