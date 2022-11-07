d3ea6ca77b9c51d3239319d383f6806cf94ce651
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-11-07 17:36:08 +0300

    BadCredentials Exception

---
+++ src/main/java/com/example/livemobileapp/exceptions/BadCredentialsException.java
@@ -0,0 +1,8 @@
+package com.example.livemobileapp.exceptions;
+
+public class BadCredentialsException extends RuntimeException{
+    public BadCredentialsException(String message) {
+        super(message);
+
+    }
+}
