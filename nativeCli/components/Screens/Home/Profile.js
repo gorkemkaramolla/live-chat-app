056ee4b0e90ea31557e24a85ea65b201c9d90184
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-12-13 02:35:55 +0300

    profile init

--- nativeCli/components/Screens/Home/Profile.js
+++ nativeCli/components/Screens/Home/Profile.js
@@ -1,10 +1,75 @@
-import {View, Text} from 'react-native';
-import React from 'react';
-
+import {View, Text, Pressable, TextInput, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {getCurrentUser} from '../../requests/UserRequest';
 export default function Profile() {
+  const [username, setUsername] = useState('');
+  const [userInformations, setUserInformations] = useState({
+    username: '',
+    email: '',
+    firstname: '',
+    lastname: '',
+    gender: '',
+  });
+
+  useEffect(() => {
+    const getUsername = async () => {
+      setUsername(await AsyncStorage.getItem('@current_user'));
+      getCurrentUser(username, response => {
+        console.debug(response);
+        setUserInformations(prev => ({
+          ...prev,
+          username: response.username,
+          email: response.email,
+          firstname: response.firstname,
+          lastname: response.lastname,
+          gender: response.gender,
+        }));
+      });
+    };
+    getUsername();
+  }, [username]);
   return (
     <View>
-      <Text>Profile</Text>
+      <Text>{userInformations.username}</Text>
+      {userInformations.firstname === null ? (
+        <TextInput
+          maxLength={12}
+          placeholder="firstname"
+          style={styles.textInput}
+          autoCapitalize={false}
+          autoCorrect={false}
+        />
+      ) : (
+        <Text>baba</Text>
+      )}
+      {userInformations.lastname === null ? (
+        <TextInput
+          maxLength={12}
+          placeholder="lastname"
+          style={styles.textInput}
+          autoCapitalize={false}
+          autoCorrect={false}
+        />
+      ) : (
+        <Text>baba</Text>
+      )}
+      {userInformations.gender === null ? (
+        <Text>gender +</Text>
+      ) : (
+        <Text>baba</Text>
+      )}
+      <Text>{userInformations.email}</Text>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  textInput: {
+    padding: 3,
+    margin: 2,
+    borderColor: 'gray',
+    borderWidth: 1,
+    minHeight: 20,
+    width: '40%',
+  },
+});
