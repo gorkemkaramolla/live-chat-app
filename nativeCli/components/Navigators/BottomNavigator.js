d67a0ba034d3a5fef4229f15101cfda529659824
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-12-13 02:35:07 +0300

    designed

--- nativeCli/components/Navigators/BottomNavigator.js
+++ nativeCli/components/Navigators/BottomNavigator.js
@@ -1,3 +1,5 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react/self-closing-comp */
 import React from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
--- nativeCli/components/Navigators/UtilNavigator.js
+++ nativeCli/components/Navigators/UtilNavigator.js
@@ -1,7 +1,9 @@
+/* eslint-disable react/self-closing-comp */
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import {ROUTES} from '../constants';
 import SearchAndFind from '../Screens/Home/Feed/SearchAndFind';
+import CommentToPost from '../Screens/Home/Posts/Comments/CommentToPost';
 const Stack = createNativeStackNavigator();
 const AuthNavigator = () => {
   return (
