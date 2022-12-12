1cde4a5bda49fcd8554063404981d16f1d6b2f9c
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-12-13 02:35:25 +0300

    user requests are added header auth

--- nativeCli/components/requests/UserRequest.js
+++ nativeCli/components/requests/UserRequest.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
+const params = new URLSearchParams();
+import {API_ROOT} from '@env';
+import qs from 'qs';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+retrieveData = async () => {
+  const value = await AsyncStorage.getItem('@access_token');
+  return value;
+};
 const registerRequest = async (email, username, password, callback) => {
   await axios
-    .post('https://live-app-rest-api.herokuapp.com/users/signup', {
+    .post(`${API_ROOT}users/signup`, {
       username: username,
       password: password,
       email: email,
@@ -14,9 +22,46 @@ const registerRequest = async (email, username, password, callback) => {
       return err.data;
     });
 };
-const getUsersReq = (page, callback) => {
+
+const loginRequest = async (username, password, callback) => {
+  const qs = require('qs');
+  await axios
+    .post(
+      `${API_ROOT}users/login`,
+      qs.stringify({username: username, password: password}),
+      {
+        headers: {'content-type': 'application/x-www-form-urlencoded'},
+      },
+    )
+    .then(response => {
+      callback(response.data);
+    })
+    .catch(err => {
+      callback(err);
+    });
+};
+//USERS
+const getCurrentUser = async (username, callback) => {
   axios
-    .get(`https://live-app-rest-api.herokuapp.com/users/${page}`)
+    .get(`${API_ROOT}users?username=${username}`, {
+      headers: {
+        Authorization: `Bearer ${await AsyncStorage.getItem('@access_token')}`,
+      },
+    })
+    .then(response => {
+      callback(response.data);
+    })
+    .catch(e => {
+      console.debug('getCurrentUser error ' + e);
+    });
+};
+const getUsersReq = async (page, callback) => {
+  axios
+    .get(`${API_ROOT}users/${page}`, {
+      headers: {
+        Authorization: `Bearer ${await AsyncStorage.getItem('@access_token')}`,
+      },
+    })
     .then(response => {
       callback(response.data);
     })
@@ -24,9 +69,13 @@ const getUsersReq = (page, callback) => {
       console.error(err);
     });
 };
-const getUserProfilePic = (username, callback) => {
+const getUserProfilePic = async (username, callback) => {
   axios
-    .get(`https://live-app-rest-api.herokuapp.com/users/image/${username}`)
+    .get(`${API_ROOT}users/image/${username}`, {
+      headers: {
+        Authorization: `Bearer ${await AsyncStorage.getItem('@access_token')}}`,
+      },
+    })
     .then(response => {
       callback(response.data);
     })
@@ -34,4 +83,10 @@ const getUserProfilePic = (username, callback) => {
       console.error(err);
     });
 };
-export {registerRequest, getUsersReq, getUserProfilePic};
+export {
+  registerRequest,
+  getUsersReq,
+  getUserProfilePic,
+  loginRequest,
+  getCurrentUser,
+};
