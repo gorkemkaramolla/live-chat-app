72aefd4efbd56d20742558ee9dbf26d91c007535
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-12-19 21:21:10 +0300

    camera update

--- nativeCli/components/Screens/Home/Message.js
+++ nativeCli/components/Screens/Home/Message.js
@@ -1,10 +1,32 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-
+import {launchCamera} from 'react-native-image-picker';
+import {launchImageLibrary} from 'react-native-image-picker';
+import {
+  SafeAreaView,
+  Pressable,
+  TextInput,
+  StyleSheet,
+  PermissionsAndroid,
+} from 'react-native';
 export default function Message() {
+  let options = {saveToPhotos: true, mediaType: 'photo'};
+
+  const openGallery = async () => {
+    const result = launchImageLibrary(options);
+  };
+  const openCamera = async () => {
+    const result = await launchCamera();
+    console.debug(result);
+  };
   return (
     <View>
-      <Text>Message</Text>
+      <Pressable onPress={openCamera}>
+        <Text>open camera</Text>
+      </Pressable>
+      <Pressable onPress={openGallery}>
+        <Text>open gallery</Text>
+      </Pressable>
     </View>
   );
 }
